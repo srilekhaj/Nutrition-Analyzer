308c56fb6a5c6ddd940a8d692919a58ed1957709
srilekhaj <jsrilekha.17@gmail.com>
2025-11-02 00:50:52 +0530

    updated changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
